ec015891785bf15037a64e140099510115ce1256
user <user@es302n02>
2018-10-02 11:04:39 +0800

    second

--- exam/src/com/fju/Hello.java
+++ exam/src/com/fju/Hello.java
@@ -4,7 +4,17 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int age=18;
+		float heigh=174.2f;
+		float weigh=63.2f;
+		
+		
+		System.out.println("age"+age);
+		System.out.println("heigh"+heigh);
+		System.out.println("weigh"+weigh);
 System.out.println("Hello");
+
+
 	}
 
 }
